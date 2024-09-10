ad2f1e8ca18c33b2e268e862fbde6c949b6a275e
Batuhan Gökdemir <79796247+batuhank0@users.noreply.github.com>
2024-09-10 22:52:19 +0300

    RepositoriesFirst

---
+++ Core/HepsiAPI.Application/Interfaces/Repositories/IReadRepository.cs
@@ -0,0 +1,29 @@
+﻿using HepsiAPI.Domain.Common;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiAPI.Application.Interfaces.Repositories
+{
+    public interface IReadRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>?> orderBy = null,
+            bool enableTracking = false);
+
+        Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+          Func<IQueryable<T>, IOrderedQueryable<T>?> orderBy = null,
+          bool enableTracking = false, int currentPage = 1, int pageSize = 3);
+        // mevcut sayfada ilk 3 veriyi alıcak currentPage yi 2 yaparsak ilk 3 değil son 3 veriyi alıyor olacak 
+
+        Task<IList<T>> GetAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            bool enableTracking = false);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+
+        Task<int> CountAysnc(Expression<Func<T, bool>>? predicate = null);
+    }
+}
