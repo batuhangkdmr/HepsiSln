82b08714be40ae722bb1b0485414129b957bbfb7
Batuhan Gökdemir <79796247+batuhank0@users.noreply.github.com>
2024-09-12 23:13:12 +0300

    MediatR Application'a eklendi

---
+++ Core/HepsiAPI.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponsive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiAPI.Application.Features.Products.Queries.GetAllProducts
+{// DTO larımızla aynı sınıfımız
+    public class GetAllProductsQueryResponsive
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
--- Presentation/HepsiAPI.Api/Program.cs
+++ Presentation/HepsiAPI.Api/Program.cs
@@ -1,4 +1,6 @@
 using Persistence;
+using HepsiAPI.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,7 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json",optional:true );
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AppApplication(); // application katmanýndan registrationa baðlandýk
 
 var app = builder.Build();
 
